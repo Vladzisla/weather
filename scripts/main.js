67efd20cb7caf1a4578658d2ccb271733dac64e2
Vlad <vladis192@gmail.com>
2021-01-13 15:57:29 +0300

    fix let

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,9 @@
 
-let API_key = '46328ad92c021e6aea74dfd00146a214'
+const API_key = '46328ad92c021e6aea74dfd00146a214'
 
 
-let list = document.getElementById('cities'),
+const list = document.getElementById('cities'),
     weatherSpan = document.getElementById('weather'),
-    descriptionSpan = document.getElementById('description'),
     temperatureSpan = document.getElementById('temperature'),
     pressureSpan = document.getElementById('pressure'),
     windSpan = document.getElementById('wind')
@@ -12,17 +11,17 @@ let list = document.getElementById('cities'),
 
 axios.get('https://gist.githubusercontent.com/alex-oleshkevich/6946d85bf075a6049027306538629794/raw/3986e8e1ade2d4e1186f8fee719960de32ac6955/by-cities.json')
     .then(response => {
-        let a = response.data[0]
+        const a = response.data[0]
 
-        for(let region of a.regions){
+        for(const region of a.regions){
 
-            let option = document.createElement('option')
+            const option = document.createElement('option')
             option.text = '---' + region.name + '---'
             option.disabled = 'disabled'
             list.append(option)
 
-            for(let city of region.cities){
-                let option = document.createElement('option')
+            for(const city of region.cities){
+                const option = document.createElement('option')
                 option.value = city.name
                 option.text = city.name
                 option.dataset.lat = city.lat
@@ -33,7 +32,7 @@ axios.get('https://gist.githubusercontent.com/alex-oleshkevich/6946d85bf075a6049
     })
 
 function getWeather() {
-    let lat = list.options[list.selectedIndex].dataset.lat,
+    const lat = list.options[list.selectedIndex].dataset.lat,
         lon = list.options[list.selectedIndex].dataset.lng
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`)
         .then(response => {
